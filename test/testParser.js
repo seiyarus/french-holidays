17db772335eb40710690d7bd1860ac1e594d4554
Julien Fournier <julien.fournier@polymont.fr>
2015-10-06 10:05:06 +0200

    algo de vérif des dates

--- test/testParser.js
+++ test/testParser.js
@@ -14,8 +14,114 @@ describe('Test getURL', function() {
 
 // Second test de la suite
 describe('Test getContenu', function() {
-        it('vérifier le contenu', function(done) {                        
+        it('vérifier le contenu', function(done) {
             assert.notEqual('', Parser.getContent()); // test de vérification du contenu non vide
             done();
         });
+});
+
+// 3e test de la suite
+describe('Test parse', function() {
+        it('vérifier le parser', function(done) {
+            assert.notEqual('', Parser.parseCalendar()); // test de vérification du contenu non vide
+            done();
+        });
+});
+
+// 4e test de la suite : une date dans les vacances scolaires toute zone
+describe('Test date vacances scolaire toutes zone', function() {
+        it('vérifier que le 17 octobre est une date de vacances toute zone', function(done) {
+          var result = Parser.isDateInHolidays('20151017', 'YYYYMMDD');
+            assert.isTrue(result); // La date du 17/10/2015 est dans les vacances toutes zone
+            done();
+        });
+
+        it('vérifier que le 17 octobre est une date de vacances zone A', function(done) {
+          var result = Parser.isDateInHolidays('20151017', 'YYYYMMDD', 'A');
+            assert.isTrue(result); // La date du 17/10/2015 est dans les vacances toutes zone
+            done();
+        });
+
+        it('vérifier que le 17 octobre est une date de vacances zone a', function(done) {
+          // Test zone en minuscule
+          var result = Parser.isDateInHolidays('20151017', 'YYYYMMDD', 'a');
+            assert.isTrue(result); // La date du 17/10/2015 est dans les vacances toutes zone
+            done();
+        });
+
+        it('vérifier que le 17 octobre est une date de vacances zone B', function(done) {
+          var result = Parser.isDateInHolidays('20151017', 'YYYYMMDD', 'B');
+            assert.isTrue(result); // La date du 17/10/2015 est dans les vacances toutes zone
+            done();
+        });
+
+        it('vérifier que le 17 octobre est une date de vacances zone b', function(done) {
+          // Test zone en minuscule
+          var result = Parser.isDateInHolidays('20151017', 'YYYYMMDD', 'b');
+            assert.isTrue(result); // La date du 17/10/2015 est dans les vacances toutes zone
+            done();
+        });
+
+        it('vérifier que le 17 octobre est une date de vacances zone C', function(done) {
+          var result = Parser.isDateInHolidays('20151017', 'YYYYMMDD', 'C');
+            assert.isTrue(result); // La date du 17/10/2015 est dans les vacances toutes zone
+            done();
+        });
+
+        it('vérifier que le 17 octobre est une date de vacances zone c', function(done) {
+          // Test zone en minuscule
+          var result = Parser.isDateInHolidays('20151017', 'YYYYMMDD', 'c');
+            assert.isTrue(result); // La date du 17/10/2015 est dans les vacances toutes zone
+            done();
+        });
+
+        it('vérifier que le 15 octobre n\'est pas une date de vacances toute zone', function(done) {
+          var result = Parser.isDateInHolidays('20151015', 'YYYYMMDD');
+            assert.isFalse(result); // La date du 15/10/2015 n'est pas dans les vacances toutes zone
+            done();
+        });
+});
+
+// 5e test de la suite : une date dans les vacances scolaires une zone spécifique
+describe('Test date vacances scolaire zone spécifique', function() {
+        it('vérifier que le 17 octobre est une date de vacances zone A', function(done) {
+          var result = Parser.isDateInHolidays('20151017', 'YYYYMMDD', 'A');
+            assert.isTrue(result); // La date du 17/10/2015 est dans les vacances toutes zone
+            done();
+        });
+
+        it('vérifier que le 13 février est une date de vacances zone A', function(done) {
+          var result = Parser.isDateInHolidays('20160213', 'YYYYMMDD', 'A');
+            assert.isTrue(result); // La date du 13/02/2016 est dans les vacances Zone A
+            done();
+        });
+
+        it('vérifier que le 6 février n\'est pas une date de vacances zone A', function(done) {
+          var result = Parser.isDateInHolidays('20160206', 'YYYYMMDD', 'A');
+            assert.isFalse(result); // La date du 06/02/2016 n'est pas dans les vacances zone A
+            done();
+        });
+
+        it('vérifier que le 13 février n\'est pas une date de vacances toute zone', function(done) {
+          var result = Parser.isDateInHolidays('20160213', 'YYYYMMDD');
+            assert.isFalse(result); // La date du 13/02/2016 n'est pas dans les vacances zone A
+            done();
+        });
+});
+
+
+// 6e test de la suite : une liste de dates dans les vacances scolaires une zone spécifique
+describe('Test liste de dates vacances scolaire zone spécifique', function() {
+        it('vérifier un ensemble de dates', function(done) {
+          var dates = new Array();;
+          dates[0] = '20160206'; // pas dans la zone A
+          dates[1] = '20160213'; // Dans la zone A
+          dates[2] = '20151017'; // Toutes zone
+          var results = Parser.isDatesInHolidays(dates, 'YYYYMMDD', 'A');
+            assert.isFalse(results[0]);
+            assert.isTrue(results[1]);
+            assert.isTrue(results[2]);
+            done();
+        });
+
 });
